14a2203643fa1b6d9d3a6b5572dd6f7c531cad16
Artiquee <amex7708@gmail.com>
2025-03-16 18:25:36 +0200

    feat: array of answers

--- frontend/src/components/QuestionItem/QuestionItem.jsx
+++ frontend/src/components/QuestionItem/QuestionItem.jsx
@@ -1,21 +1,40 @@
 import { FaTrash } from "react-icons/fa";
 
-const QuestionItem = ({ id, index, handleRemoveQuestion}) => {
+const QuestionItem = ({ id, index, answers, handleRemoveQuestion}) => {
+
+    const filteredAnswers = answers.filter(answer => answer.questionId === id);
+
     return (
-        <div className="d-flex align-items-center gap-5">
-            <span>{index+1}</span>
-            <div className="d-flex align-items-center">
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Question</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1"/>
+        <div className="border border-2 m-2 p-2">
+            <div className="d-flex align-items-center gap-5">
+                <span>{index+1}</span>
+                <div className="d-flex align-items-center">
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Question</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1"/>
+                    </div>
+                    <select name="type">
+                        <option value="text">Text</option>
+                        <option value="single choice">Single Choice</option>
+                        <option value="multiple choices">Multiple Choices</option>
+                    </select>
                 </div>
-                <select name="type">
-                    <option value="text">Text</option>
-                    <option value="single choice">Single Choice</option>
-                    <option value="multiple choices">Multiple Choices</option>
-                </select>
+                <FaTrash size={30} color="red" cursor="pointer" onClick={() => handleRemoveQuestion(id)}/>
+            </div>  
+            <div>
+                <span>Answers</span>
+                {filteredAnswers?.map((answer, index) => (
+                    <ul className="d-flex align-items-center gap-4">
+                        <span>{index}</span>
+                        <li key={answer.id} className="mb-3">
+                            <div>
+                                <label htmlFor="exampleInputEmail1" className="form-label">Choice</label>
+                                <input type="text" className="form-control" id="exampleInputEmail1"/>
+                            </div>
+                        </li>
+                    </ul>
+                ))}
             </div>
-            <FaTrash size={30} color="red" cursor="pointer" onClick={() => handleRemoveQuestion(id)}/>
         </div>
     )
 }
--- frontend/src/components/QuestionList/QuestionList.jsx
+++ frontend/src/components/QuestionList/QuestionList.jsx
@@ -1,5 +1,4 @@
 import {useState} from "react";
-import {FaTrash} from "react-icons/fa";
 import { QuestionItem } from "../QuestionItem/QuestionItem";
 
 const QuestionList = () => {
@@ -8,28 +7,39 @@ const QuestionList = () => {
         question: 'Question 1',
         type: 'Text'
     }]);
+    const [answers, setAnswers] = useState([
+        { id: 1, questionId: 1, answer: 'Answer 1' },
+    ]);
 
     const handleAddQuestion = () => {
         setQuestions((prevState) => [...prevState, {
             id: questions.length + 1,
             question: `Question ${questions.length + 1}`,
             type: 'Text'
+        }]);
+        setAnswers((prevState) => [...prevState, {
+            id: answers.length + 1,
+            question: `Question ${answers.length + 1}`,
+            questionId: questions.length + 1,
+            answer: ''
         }])
     }
 
     const handleRemoveQuestion = (id) => {
         setQuestions(prevState =>  prevState.filter(question => question.id !== id))
+        setAnswers(prevState => prevState.filter(answer => answer.questionId !== id));
     }
 
     return (
         <form>
             <button type="button" className="btn btn-primary" onClick={handleAddQuestion}>Add Question</button>
             <div>
-                {questions?.map((question, index) => 
+                {questions?.map(({ id }, index) => 
                     <QuestionItem 
-                        key={question.id} 
-                        id={question.id} 
+                        key={id} 
+                        id={id} 
                         index={index}
+                        answers={answers}
                         handleRemoveQuestion={handleRemoveQuestion}
                     />)}
             </div>
