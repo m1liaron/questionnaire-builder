666b7f70766d904d97534cb0fd81cf5fdd8e40ae
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-03-17 10:10:34 +0200

    feat: quiz actions modal
    Took 31 minutes

--- frontend/src/components/QuizItem/QuizItem.jsx
+++ frontend/src/components/QuizItem/QuizItem.jsx
@@ -1,17 +1,42 @@
 import { BsThreeDotsVertical } from "react-icons/bs";
+import {useState} from "react";
+import {Button, Modal} from "react-bootstrap";
 
 const QuizItem = ({ name, description, questionsAmount = 1 }) => {
-    return (
-        <div className="border border-3 p-3 " style={{ minWidth: "400px" }}>
-            <div className="d-flex justify-content-between align-items-center">
-                <h3>{name}</h3>
-                <BsThreeDotsVertical cursor="pointer"/>
-            </div>
-            <p>{description}</p>
+    const [showActionsModal, setShowActionsModal] = useState(false);
 
-            <p>Questions: {questionsAmount}</p>
-        </div>
+    return (
+        <>
+            <div className="border border-3 p-3 " style={{minWidth: "400px"}}>
+                <div className="d-flex justify-content-between align-items-center">
+                    <h3>{name}</h3>
+                    <BsThreeDotsVertical cursor="pointer" onClick={() => setShowActionsModal(!showActionsModal)}/>
+                </div>
+                <p>{description}</p>
+
+                <p>Questions: {questionsAmount}</p>
+            </div>
+            {showActionsModal && (
+                    <div
+                        className="modal"
+                        style={{display: 'block', position: 'absolute'}}
+                    >
+                        <Modal.Dialog>
+                            <Modal.Header closeButton onClick={() => setShowActionsModal(false)}>
+                                <Modal.Title>Actions: {name}</Modal.Title>
+                            </Modal.Header>
+
+                            <Modal.Body className="d-flex gap-4">
+                                <Button variant="primary">Edit</Button>
+                                <Button variant="secondary">Run</Button>
+                                <Button variant="warning">Remove</Button>
+                            </Modal.Body>
+                        </Modal.Dialog>
+                    </div>
+            )}
+
+        </>
     )
 }
 
-export { QuizItem };
+export {QuizItem};
