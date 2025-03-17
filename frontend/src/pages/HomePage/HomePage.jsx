c659358ec262fd80af7ac3bb2f088844e099c178
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-03-17 21:46:23 +0200

    feat: modal for sort buttons
    Took 9 minutes

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -3,18 +3,32 @@ import {Button} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {AppPath} from "../../common/enums/AppPath.js";
 import { MdOutlineSort } from "react-icons/md";
+import {useState} from "react";
 
 const HomePage = () => {
+    const [showSortModal, setShowSortModal] = useState(false);
+
     return (
         <div className="p-3">
             <header className="d-flex justify-content-between">
                 <h1>Quiz Catalog</h1>
                 <div>
-                    <Button>
+                    <Button onClick={() => setShowSortModal(!showSortModal)}>
                         <MdOutlineSort size={20} />
                     </Button>
                 </div>
             </header>
+
+            {showSortModal && (
+                <div className="position-absolute border border-2 p-3" style={{right: 0, background: "white"}}>
+                    <span>Sort by</span>
+                    <div className="d-flex flex-column gap-1">
+                        <Button>Name</Button>
+                        <Button>Amount Of Questions</Button>
+                        <Button>Amount Of Completion</Button>
+                    </div>
+                </div>
+            )}
 
             <Link to={AppPath.CreateQuiz}>
                 <Button>Create Quiz</Button>
@@ -24,4 +38,4 @@ const HomePage = () => {
     )
 }
 
-export {HomePage };
+export {HomePage};
